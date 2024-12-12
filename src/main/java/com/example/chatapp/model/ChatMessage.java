1c8e8e4351e0e01456eba4cfb13ab73ac9d1f2fc
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2024-12-12 16:01:41 +0200

    ChatMessage class and MessageType enum created

---
+++ src/main/java/com/example/chatapp/model/ChatMessage.java
@@ -0,0 +1,20 @@
+package com.example.chatapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private String content;
+
+    private String sender;
+
+    private MessageType type;
+
+}
---
+++ src/main/java/com/example/chatapp/model/MessageType.java
@@ -0,0 +1,5 @@
+package com.example.chatapp.model;
+
+public enum MessageType {
+    CHAT, JOIN, LEAVE
+}
