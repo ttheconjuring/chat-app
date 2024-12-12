203d422b27e8d0d8454afe14576815c9fb66d11e
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2024-12-12 16:49:33 +0200

    main.js added to the page

--- src/main/java/com/example/chatapp/config/WebSocketEventListener.java
+++ src/main/java/com/example/chatapp/config/WebSocketEventListener.java
@@ -10,8 +10,6 @@ import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
-import java.util.Objects;
-
 @Component
 @RequiredArgsConstructor
 @Slf4j
